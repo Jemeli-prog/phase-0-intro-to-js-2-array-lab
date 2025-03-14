932aa0af6e1c31077ffd726159c183cf42f35f1f
Ruth Kiptoo <ruthkiptoo523@gmail.com>
2025-03-14 21:11:01 +0300

    added assignments

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+
+    const cats = ["Milo", "Otis", "Garfield"];
+
+
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+    console.log(cats);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    console.log(cats);
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+
+}
+function appendCat(coolCats){
+    return [...cats, "Broom"];
+    
+
+}
+
+function prependCat(bestCats){
+    return ["Arnold", ...cats];
+}
+
+
+function removeLastCat(Garfield){
+ return cats.slice(0, cats.length -1);
+}
+function removeFirstCat(milo){
+    return cats.slice(1);
+}
+
+
